db13739ddd3c7be5962f598a27e759ad69cefbe9
Talal Abuzinadah <primezinadah@gmail.com>
2025-11-11 01:50:50 +0000

    Add data download script

---
+++ download-data.sh
@@ -0,0 +1,4 @@
+# Download Hotpot Data
+wget -P ./data http://curtis.ml.cmu.edu/datasets/hotpot/hotpot_dev_distractor_v1.json
+wget -P ./data http://curtis.ml.cmu.edu/datasets/hotpot/hotpot_dev_fullwiki_v1.json
+wget -P ./data http://curtis.ml.cmu.edu/datasets/hotpot/hotpot_train_v1.1.json
