ee011dd90f82ce89040e10d3b57f8459aea4734f
Talal Abuzinadah <primezinadah@gmail.com>
2025-11-12 18:30:03 +0000

    Add an async basic answering pipeline

--- src/hotpotqa_group_d/main.py
+++ src/hotpotqa_group_d/main.py
@@ -1,4 +1,6 @@
-from hotpotqa_group_d.pipelines import basic_answer
+import asyncio
+
+from hotpotqa_group_d.pipelines import async_answer
 
 if __name__ == "__main__":
-    basic_answer("results/baseline.json")
+    asyncio.run(async_answer("results/baseline.json"))
--- src/hotpotqa_group_d/pipelines/answering_pipelines.py
+++ src/hotpotqa_group_d/pipelines/answering_pipelines.py
@@ -1,5 +1,8 @@
+import asyncio
+
 from hotpotqa_group_d.config import Env
 from hotpotqa_group_d.services import (
+    async_prompt_mistral,
     create_client,
     format_results,
     parse_data,
@@ -30,4 +33,35 @@ def basic_answer(result_path):
         answer = prompt_mistral(client, question)
         qa_pairs.append((question, answer))
 
+    # Filter out errored results
+    successful_pairs = [pair for pair in qa_pairs if pair[1] != ""]
+
     format_results(qa_pairs, file_path=result_path)
+
+
+async def async_answer(result_path):
+    """
+    Improvement on simple answer by making the calls async for better performance
+
+    Args:
+        result_path (str): File path to write results
+    """
+
+    env = Env()
+    client = create_client(env.MISTRAL_KEY)
+    dev_fullwiki_data = parse_data()
+
+    # Create a list of concurrent tasks
+    tasks = []
+    for data_point in dev_fullwiki_data:
+        question = data_point["question"]
+        tasks.append(async_prompt_mistral(client, question))
+
+    # Run all tasks in parallel
+    print("Answering questions in parallel")
+    qa_pairs = await asyncio.gather(*tasks)
+
+    # Filter out errored results
+    successful_pairs = [pair for pair in qa_pairs if pair[1] != ""]
+
+    format_results(successful_pairs, file_path="results/baseline_async.json")
