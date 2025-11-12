1c0df3d9135651f576a53271170db280cf818939
Talal Abuzinadah <primezinadah@gmail.com>
2025-11-12 17:22:52 +0000

    Refactor to use once client in pipeline instead of a new one with every prompt

--- src/hotpotqa_group_d/main.py
+++ src/hotpotqa_group_d/main.py
@@ -1,10 +1,16 @@
 from hotpotqa_group_d.config import Env
-from hotpotqa_group_d.services import format_results, parse_data, prompt_mistral
+from hotpotqa_group_d.services import (
+    create_client,
+    format_results,
+    parse_data,
+    prompt_mistral,
+)
 
 if __name__ == "__main__":
 
     # Simple answers for baseline evaluation
     env = Env()
+    client = create_client(env.MISTRAL_KEY)
     dev_fullwiki_data = parse_data()
 
     # Results
@@ -15,7 +21,7 @@ if __name__ == "__main__":
         print(f"asnwering {idx + 1}/{len(dev_fullwiki_data)}")
 
         question = data_point["question"]
-        answer = prompt_mistral(env.MISTRAL_KEY, question)
+        answer = prompt_mistral(client, question)
         qa_pairs.append((question, answer))
 
     format_results(qa_pairs, file_path="results/baseline.json")
