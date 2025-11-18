31af320d6ae9871d53b18fb6d4a985fb752a4f95
Talal Abuzinadah <primezinadah@gmail.com>
2025-11-18 14:20:51 +0000

    Add baseline expirement

--- src/hotpotqa_group_d/experiments/baseline.py
+++ src/hotpotqa_group_d/experiments/baseline.py
@@ -1,4 +1,4 @@
 from hotpotqa_group_d.pipelines import basic_answer
 
 if __name__ == "__main__":
-    basic_answer("50-questions-sample.json", sample_size=50)
+    basic_answer("results/baseline.json", sample_size=100)
