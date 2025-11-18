83bc08da602d4584d5422cf58fefd372fe21ae78
Talal Abuzinadah <primezinadah@gmail.com>
2025-11-18 14:54:40 +0000

    Add evaluation script for predictions

---
+++ scripts/evaluate-result.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Takes the results name and evaluates it
+
+# Extract the filepath from its name
+FILEPATH="./results/$1.json"
+
+echo evaluating $FILEPATH
+
+# Output filepath
+OUTPUT_PATH="./src/hotpotqa_group_d/evaluation/$1-results.txt"
+
+# Run the script and extract last line to the results to ignore error messages
+python ./src/hotpotqa_group_d/evaluation/evaluation.py $FILEPATH ./data/hotpot_dev_fullwiki_v1.json | tail -n 1 >$OUTPUT_PATH
+
+echo Saved results of $FILEPATH to $OUTPUT_PATH
