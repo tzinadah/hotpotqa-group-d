167900933339b2ba8c9c16abe345d77e466de6ff
Talal Abuzinadah <primezinadah@gmail.com>
2025-11-24 22:10:39 +0000

    Create expirements for templates

--- src/hotpotqa_group_d/config/__init__.py
+++ src/hotpotqa_group_d/config/__init__.py
@@ -1,3 +1,8 @@
 from .consts import Model
 from .env import Env
-from .prompt_templates import vanilla_template
+from .prompt_templates import (
+    blackmail_template,
+    expert_template,
+    polite_template,
+    vanilla_template,
+)
---
+++ src/hotpotqa_group_d/experiments/templates.py
@@ -0,0 +1,40 @@
+"""
+This expirement is to test different approaches with templating the prompt
+
+1- Asking the LLM to act as an expert
+2- Asking the LLM politely
+3- Asking the LLM with emotional blackmail
+"""
+
+from hotpotqa_group_d.config import (
+    Model,
+    blackmail_template,
+    expert_template,
+    polite_template,
+)
+from hotpotqa_group_d.pipelines import templated_answer
+
+if __name__ == "__main__":
+    # Expert expirement
+    templated_answer(
+        "results/expert-prompt.json",
+        template=expert_template,
+        model=Model.MEDIUM,
+        sample_size=100,
+    )
+
+    # Polite expirement
+    templated_answer(
+        "results/polite-prompt.json",
+        template=polite_template,
+        model=Model.MEDIUM,
+        sample_size=100,
+    )
+
+    # Blackmail expirement
+    templated_answer(
+        "results/blackmail-prompt.json",
+        template=blackmail_template,
+        model=Model.MEDIUM,
+        sample_size=100,
+    )
