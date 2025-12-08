41d31a09f298bdf49def3437de79974c8cfd74e8
Talal Abuzinadah <primezinadah@gmail.com>
2025-12-08 23:53:09 +0000

    Write expirement for reasoning

---
+++ src/hotpotqa_group_d/experiments/reasoning.py
@@ -0,0 +1,21 @@
+"""
+Experiment to test both in prompt reasoning and reasoning models
+"""
+
+from hotpotqa_group_d.config import Model, reasoning_template
+from hotpotqa_group_d.pipelines import RAG_answer
+
+if __name__ == "__main__":
+    # In prompt reasoning
+    RAG_answer(
+        "results/in-prompt-reasoning.json",
+        model=Model.MEDIUM,
+        sample_size=100,
+        template=reasoning_template,
+        top_k=30,
+    )
+
+    # Reasoning model
+    RAG_answer(
+        "results/reasoning-model.json", model=Model.REASONING, sample_size=100, top_k=30
+    )
