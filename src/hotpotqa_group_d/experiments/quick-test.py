3a02dc8077cb02b0cbbf93d4209b6ab9ba83e1e2
Talal Abuzinadah <primezinadah@gmail.com>
2025-12-08 19:26:51 +0000

    Remove temp experiment

--- src/hotpotqa_group_d/experiments/quick-test.py
+++
@@ -1,7 +0,0 @@
-from hotpotqa_group_d.config.consts import Model
-from hotpotqa_group_d.pipelines.answering_pipelines import RAG_answer
-
-if __name__ == "__main__":
-    RAG_answer(
-        "results/temp.json", "./chroma_db", Model.LARGE, sample_size=20, top_k=30
-    )
