c7337f8331b643b32a0a6d1c05ab2b443dc3a632
Talal Abuzinadah <primezinadah@gmail.com>
2025-12-09 11:49:32 +0000

    Remove the quick test file

--- src/hotpotqa_group_d/experiments/quick-test.py
+++
@@ -1,9 +0,0 @@
-from hotpotqa_group_d.config import Env, Model
-from hotpotqa_group_d.services import create_client, reasoning_prompt_mistral
-
-if __name__ == "__main__":
-    env = Env()
-
-    client = create_client(env.MISTRAL_KEY)
-
-    print(reasoning_prompt_mistral(client, "Hi", Model.REASONING))
