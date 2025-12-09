3e597041cf480b3a154d39a2241692231cf57abf
Talal Abuzinadah <primezinadah@gmail.com>
2025-12-09 20:30:01 +0000

    Extract results and compare rag

--- src/hotpotqa_group_d/experiments/visualise.py
+++ src/hotpotqa_group_d/experiments/visualise.py
@@ -5,9 +5,29 @@ if __name__ == "__main__":
 
     # Extract results
     baseline_results = extract_results("baseline", "results/baseline-results.json")
+
+    small_rag_results = extract_results("small rag", "results/small-model-results.json")
     medium_rag_results = extract_results(
         "medium rag", "results/medium-model-results.json"
     )
+    large_rag_results = extract_results("large rag", "results/large-model-results.json")
 
-    # Plot
-    plot_metrics([baseline_results, medium_rag_results], "plots/medium-rag.pdf")
+    blackmail_template_results = extract_results(
+        "blackmail template", "results/blackmail-prompt-results.json"
+    )
+    polite_template_results = extract_results(
+        "polite template", "results/polite-prompt-results.json"
+    )
+    expert_template_results = extract_results(
+        "expert template", "results/expert-prompt-results.json"
+    )
+
+    prompt_reasoning_results = extract_results(
+        "in-prompt reasoning", "results/in-prompt-reasoning-results.json"
+    )
+    model_reasoning_results = extract_results(
+        "model reasoning", "results/reasoning-model-results.json"
+    )
+
+    # Plot RAG comparison
+    plot_metrics([baseline_results, medium_rag_results], "plots/rag-comparison.pdf")
