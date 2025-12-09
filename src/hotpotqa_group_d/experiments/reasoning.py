06fd589a0fe93a7faaa56afe0f8271d1bdb759e3
Talal Abuzinadah <primezinadah@gmail.com>
2025-12-09 16:23:41 +0000

    Uncomment first half of experiment

--- src/hotpotqa_group_d/experiments/reasoning.py
+++ src/hotpotqa_group_d/experiments/reasoning.py
@@ -2,18 +2,18 @@
 Experiment to test both in prompt reasoning and reasoning models
 """
 
-from hotpotqa_group_d.config import Model
+from hotpotqa_group_d.config import Model, reasoning_template
 from hotpotqa_group_d.pipelines import RAG_answer
 
 if __name__ == "__main__":
     # In prompt reasoning
-    # RAG_answer(
-    #     "results/in-prompt-reasoning.json",
-    #     model=Model.MEDIUM,
-    #     sample_size=100,
-    #     template=reasoning_template,
-    #     top_k=30,
-    # )
+    RAG_answer(
+        "results/in-prompt-reasoning.json",
+        model=Model.MEDIUM,
+        sample_size=100,
+        template=reasoning_template,
+        top_k=30,
+    )
 
     # Reasoning model
     RAG_answer(
